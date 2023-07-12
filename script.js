46116523ac0a06cf7adf6ee0bd881439ddf2764d
Natasha Lemak <natasha.lemak@gmail.com>
2023-07-12 16:42:48 -0700

    Change color of square on hover

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const GRID_SIZE = 16;
 
+function onHover(event) {
+  this.classList.add('sketched');
+}
+
 const grid_container = document.querySelector('#grid-container');
 grid_container.style.gridTemplateColumns = `repeat(${GRID_SIZE}, 1fr)`;
 
 for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
+  square.addEventListener('mouseenter', onHover);
   grid_container.appendChild(square);
 }
